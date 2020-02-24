566727de9546ddff0a642fc8d76fd5cb8acc9877
jonathan1murphy <61337419+jonathan1murphy@users.noreply.github.com>
2020-02-24 17:31:49 -0500

    Add files via upload

---
+++ Address.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include "Address.h"
+using namespace std;
+
+Address::Address(string streetAddress, string city, string state, string zipCode)
+{
+    setAddressStreet (string);
+    setAddressCity (string);
+    setAddressState (string);
+    setAddressZipCode (string);
+}
+
+Address::Address(){
+    addressStreet = "";
+    addressCity = "";
+    addressState = "";
+    addressZipCode = "";
+}
+
+void Address::setAddressStreet (string streetAddress)
+{
+    AddressStreet=streetAddress;
+}
+string Address::getAddressStreet()
+{
+    return AddressStreet;
+}
+
+void Address::setAddressCity (string city)
+{
+    AddressCity=city;
+}
+string Address::getAddressCity()
+{
+    return AddressCity;
+}
+
+void Address::setAddressState (string state)
+{
+    AddressState=state;
+}
+string Address::getAddressState()
+{
+    return AddressState;
+}
+
+void Address::setAddressZipCode (string zipCode)
+{
+    AddressZipCode=zipCode;
+}
+string Address::getAddressZipCode()
+{
+    return AddressZipCode;
+}
