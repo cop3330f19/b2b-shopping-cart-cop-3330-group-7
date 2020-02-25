f5817f0b5283175cf91b7e029cc765b33a70f791
Muhammad1368 <61439594+Muhammad1368@users.noreply.github.com>
2020-02-25 14:19:47 -0500

    Update AleemCustomer.h

--- AleemCustomer.h
+++ AleemCustomer.h
@@ -1,10 +1,11 @@
 class Customer{
-    public:
+    private:
     std::string customerNumber;
     std::string customerName;
     double lineOfCredit;
     Address * corperateAddress;
     
+    public:
     std::string setCustomerName (std::string cName);
     std::string setCustomerNumber (std::string cNum);
     double setLineOfCredit (double loCredit);
