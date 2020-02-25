aaae84b425550872a882b8b9d6a3bc60b64a4023
Muhammad1368 <61439594+Muhammad1368@users.noreply.github.com>
2020-02-24 21:35:52 -0500

    Create AleemProduct.h

---
+++ AleemProduct.h
@@ -0,0 +1,12 @@
+class Product{
+    public:
+    int itemNo;
+    std::string description;
+    double price;
+    int stockQuantity
+    
+    int setItemNo (int iNum);
+    std::string setDescription (std::string Description);
+    double setPrice (double Price);
+    int setStockQuantity (int StockQuantity);
+}
