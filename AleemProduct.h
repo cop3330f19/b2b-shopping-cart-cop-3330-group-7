0dd2abc084f8063849aa60f3f6f8c3e95051806a
Muhammad1368 <61439594+Muhammad1368@users.noreply.github.com>
2020-02-25 14:20:18 -0500

    Update AleemProduct.h

--- AleemProduct.h
+++ AleemProduct.h
@@ -1,10 +1,11 @@
 class Product{
-    public:
+    private:
     int itemNo;
     std::string description;
     double price;
     int stockQuantity
     
+    public:
     int setItemNo (int iNum);
     std::string setDescription (std::string Description);
     double setPrice (double Price);
