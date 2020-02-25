c1ce4ef7f2219e7c5c267286b4bf17d9b4c0cf99
Muhammad1368 <61439594+Muhammad1368@users.noreply.github.com>
2020-02-24 21:34:11 -0500

    Create AleemAddress.h

---
+++ AleemAddress.h
@@ -0,0 +1,13 @@
+class Address{
+    public:
+    std::string streetAddress;
+    std::string city;
+    std::string state;
+    std::string zipCode;
+    
+    std::string setStreetAddress (std::string sAddress);
+    std::string setCity (std::string City);
+    std::string setState (std::string State);
+    std::string setZipCode (std::string zCode);
+}
+
